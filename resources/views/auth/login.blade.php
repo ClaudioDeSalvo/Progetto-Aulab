2d0dd199aab8a7126ecbb74ca2f612201a8b5c3a
asprenolapo <aslape.lapo@gmail.com>
2024-05-16 17:57:46 +0200

    scaffolding completato + installato fortify + creata auth + viste login/register + collegamenti alla navbar

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+
+    <div class="container-fluid bg-success">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center">
+                <h1>Accedi</h1>
+                <h2>sotto titolo home </h2>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid mt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-4">
+                <form>
+                    <div class="mb-3">
+                        <label class="form-label">Indirizzo Mail</label>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Ricordami</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Invia</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+
+<div class="container-fluid bg-success">
+    <div class="row justify-content-center">
+        <div class="col-12 text-center">
+            <h1>Home page</h1>
+            <h2>sotto titolo home </h2>
+        </div>
+    </div>
+</div>
+
+
+</x-layout>
