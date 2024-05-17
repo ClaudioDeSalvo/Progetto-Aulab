15c36e43218bba48ad24eca1e4f50ef35c1c884c
asprenolapo <aslape.lapo@gmail.com>
2024-05-17 10:42:11 +0200

    test commento appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $categories = Category::all();
-        View::share('categories', $categories);
+        // $categories = Category::all();
+        // View::share('categories', $categories);
     }
 }
