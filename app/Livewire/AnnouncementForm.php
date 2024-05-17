da82aa5365ce975da8ae521e23ff5765a7149996
asprenolapo <aslape.lapo@gmail.com>
2024-05-17 19:28:11 +0200

    validazione form annoucement completata ma da ricontrollare e link storage con cartella img creata

--- app/Livewire/AnnouncementForm.php
+++ app/Livewire/AnnouncementForm.php
@@ -4,35 +4,54 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Announcement;
-use App\Models\Category;
+use Livewire\Attributes\Validate;
+
 
 class AnnouncementForm extends Component
 {
-    public $title;
-    public $subtitle;
-    public $body;
-    public $price;
-    public $img;
 
-    public $category_id = [];
+
+    #[Validate('required|min:5')]
+    public $title;
+    #[Validate('required|min:5')]
+    public $subtitle;
+    #[Validate('required|min:15')]
+    public $body;
+
+
+
+
+        protected $messages = [
+        'title.min' => 'Il titolo e troppo corto',
+        'subtitle.min' => 'Il sottotitolo e troppo corto',
+        'body.min' => 'Il corpo e troppo corto',
+        'price' => 'Il prezzo e troppo basso',
+    ];
+
+    public function updated($propertyName){
+    $this->validateOnly($propertyName);
+    }
+
+
+
+   // public $category_id = [];
 
     public function save()
     {
-
+        $this->validate();
         Announcement::create([
-
             'title' => $this->title,
             'subtitle' => $this->subtitle,
             'body' => $this->body,
             'price' => $this->price,
             'user_id' => auth()->user()->id,
             'category_id' => $this->category_id,
-            // 'img' => $this->img,
+            //'img'=>$this->img->has('img') ? $this->file('img')->store('/public/img') : '/img/annunciodefault.jpg',
 
         ]);
 
         $this->reset();
-        session()->flash('status', 'Annuncio inserito con successo!');
+        session()->flash('message', 'Annuncio inserito con successo!');
     }
 
     public function render()
--- resources/views/livewire/announcement-form.blade.php
+++ resources/views/livewire/announcement-form.blade.php
@@ -2,9 +2,9 @@
     <div class="row justify-content-center">
         <div class="col-12">
 
-            @if (session('status'))
+            @if (session('message'))
                 <div class="alert alert-success" id="session1">
-                    {{ session('status') }}
+                    {{ session('message') }}
                 </div>
             @endif
 
@@ -12,32 +12,43 @@
                 @csrf
                 <div class="mb-3">
                     <label class="form-label">Titolo</label>
-                    <input wire:model="title" type="text" class="form-control" name="title">
+                    <input wire:model="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title">
+                    <div>
+                        @error('title') <span class="error bg-danger">{{ $message }}</span> @enderror
+                    </div>
                 </div>
                 <div class="mb-3">
                     <label class="form-label">Sottotitolo</label>
-                    <input wire:model="subtitle" type="text" class="form-control" name="subtitle">
+                    <input wire:model="subtitle" type="text" class="form-control @error('subtitle') is-invalid @enderror" name="subtitle">
+                    <div>
+                        @error('subtitle') <span class="error bg-danger">{{ $message }}</span> @enderror
+                    </div>
                 </div>
-                <div class="mb-3 form-check">
+                <div class="mb-3">
                     <label class="form-label">Corpo del testo</label>
-                    <textarea class="form-control" name="body" wire:model="body" id="" cols="30" rows="10"></textarea>
+                    <textarea class="form-control @error('body') is-invalid @enderror" name="body" wire:model="body" id="" cols="30" rows="10"></textarea>
+                    <div>
+                        @error('body') <span class="error bg-danger">{{ $message }}</span> @enderror
+                    </div>
                 </div>
-                <div class="mb-3 form-check">
+                <div class="mb-3">
                     <label class="form-label">Prezzo</label>
-                    <input wire:model="price" type="text" class="form-control" name="price">
+                    <input wire:model="price" type="text" class="form-control @error('price') is-invalid @enderror" name="price">
+                    <div>
+                        @error('price') <span class="error bg-danger">{{ $message }}</span> @enderror
+                    </div>
                 </div>
-                <div class="mb-3 form-check">
+                <div class="mb-3">
                     <label class="form-label">Img</label>
-                    <input wire:model="img" type="file" class="form-control" name="img">
+                    <input wire:model="img" type="file" class="form-control" name="img">                   
                 </div>
-                <div class="mb-3 form-check">
+                <div class="mb-3">
                     <select wire:model="category_id">
                         @foreach ($categories as $category)
                             <option value="{{ $category->id }}">{{ $category->name }}</option>
                         @endforeach
                     </select>
                 </div>
-
                 <button type="submit" class="btn btn-primary">Invia</button>
             </form>
         </div>
